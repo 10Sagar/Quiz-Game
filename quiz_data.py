396bec59ba45e78a0019e0ffa833f5c69bf11d8b
Sagar Nailwal <90567257+10Sagar@users.noreply.github.com>
2022-05-08 16:35:01 +0530

    Add files via upload

---
+++ quiz_data.py
@@ -0,0 +1,10 @@
+import requests
+
+parameters = {
+    "amount": 10,
+    "type": "multiple"
+}
+
+response = requests.get(url="https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple", params=parameters)
+question_data = response.json()["results"]
+
